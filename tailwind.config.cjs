2ea5883877c4eadc5bc231ceca1e787c0b1c1208
Zain Wania <zain@wania.dev>
2022-11-01 17:42:25 -0400

    test no font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,6 @@ module.exports = {
         "t-black": "#121212",
       },
       fontFamily: {
-        satoshi: ["Satoshi", ...defaultTheme.fontFamily.sans],
         hack: ["Hack", ...defaultTheme.fontFamily.mono],
       },
     },
